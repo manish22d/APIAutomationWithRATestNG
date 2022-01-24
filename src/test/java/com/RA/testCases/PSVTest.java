458b65d737548968f481dc4c45dd75f7de32f4e6
Manish Kumar <mr.manish22d@gmail.com>
2022-01-24 23:49:50 +0530

    updated method : Manish

--- src/test/java/com/RA/testCases/PSVTest.java
+++ src/test/java/com/RA/testCases/PSVTest.java
@@ -14,6 +14,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -26,23 +27,25 @@ import com.opencsv.CSVParserBuilder;
 import com.opencsv.CSVReader;
 import com.opencsv.CSVReaderBuilder;
 
-public class PSVTest extends BaseClass{
-	CSVReader csvReader ;
+public class PSVTest extends BaseClass {
+	CSVReader csvReader;
 	List<String[]> allPSVData;
-	
+	FTPUtils ftp = new FTPUtils();
+
 	@BeforeClass
 	public void setUpFiles() throws IOException, URISyntaxException {
-		Reader reader = Files
-				.newBufferedReader(Paths.get(ClassLoader.getSystemResource("psv/test.psv").toURI()));
+
+		ftp.connect();
+		Reader reader = Files.newBufferedReader(Paths.get(ClassLoader.getSystemResource("psv/test.psv").toURI()));
+		// below line will be used for ftp file
+//		Reader reader = new InputStreamReader(ftp.readFile("/download/version.txt"));
 		CSVParser parser = new CSVParserBuilder().withSeparator('|').withIgnoreQuotations(true).build();
 
 		csvReader = new CSVReaderBuilder(reader).withSkipLines(0).withCSVParser(parser).build();
-		
+
 		allPSVData = csvReader.readAll();
-		
-		
 	}
-	
+
 	@Test
 	public void verifyHeaderInPSV(Method method) {
 		extentTest = extent.startTest(method.getName());
@@ -56,47 +59,46 @@ public class PSVTest extends BaseClass{
 				.collect(Collectors.toList());
 		System.out.println(headers);
 		System.out.println(headerInPSV.containsAll(headers));
-		assertThat("",headerInPSV.containsAll(headers));
+		assertThat("", headerInPSV.containsAll(headers));
 	}
-	
+
 	@Test
 	public void validateData(Method method) {
 		extentTest = extent.startTest(method.getName());
-		
+
 		List<String> data = Arrays.asList(allPSVData.get(1));
 		data.forEach(System.out::println);
-		
+
 	}
-	
+
 	@Test
 	public void testFTP(Method method) throws IOException {
 		extentTest = extent.startTest(method.getName());
-		
-		FTPUtils ftp = new FTPUtils();
-		ftp.connect();
-		
+
 		System.out.println(ftp.listDir("/download"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(ftp.readFile("/download/version.txt")));
 		try {
-		    StringBuilder sb = new StringBuilder();
-		    String line = br.readLine();
+			StringBuilder sb = new StringBuilder();
+			String line = br.readLine();
 
-		    while (line != null) {
-		        sb.append(line);
-		        sb.append(System.lineSeparator());
-		        line = br.readLine();
-		    }
-		    String everything = sb.toString();
-		    System.out.println(everything);
+			while (line != null) {
+				sb.append(line);
+				sb.append(System.lineSeparator());
+				line = br.readLine();
+			}
+			String everything = sb.toString();
+			System.out.println(everything);
 		} finally {
-		    br.close();
+			br.close();
 		}
 //		ftp.changeDirectory("/download");
 //		System.out.println(ftp.listDir("/"));
-		ftp.close();
-		
+
 	}
-	
-	
+
+	@AfterClass
+	public void closeConnections() throws IOException {
+		ftp.close();
+	}
 
 }
